10ce387d3c216fae0c0c01b477a774fb623349fe
glsunnykumar <glsunny.kumar@gmail.com>
2025-10-23 19:27:29 +0530

    Enhanching the design

--- src/app/core/pages/dashboard/dashboard.component.ts
+++ src/app/core/pages/dashboard/dashboard.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
@@ -29,25 +29,42 @@ interface Order {
     RouterLinkActive,
   ],
   templateUrl: './dashboard.component.html',
-  styleUrl: './dashboard.component.scss'
+  styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
-   sidebarOpen = true;
- displayedColumns: string[] = ['id', 'customer', 'total', 'status'];
+  sidebarOpen = true;
+   isMobile = false;
+  displayedColumns: string[] = ['id', 'customer', 'total', 'status'];
 
   orders: Order[] = [
     { id: 'ORD-1001', customer: 'Ravi Sharma', total: 2500, status: 'pending' },
-    { id: 'ORD-1002', customer: 'Sneha Patel', total: 1800, status: 'delivered' },
-    { id: 'ORD-1003', customer: 'Amit Verma', total: 3500, status: 'cancelled' },
+    {
+      id: 'ORD-1002',
+      customer: 'Sneha Patel',
+      total: 1800,
+      status: 'delivered',
+    },
+    {
+      id: 'ORD-1003',
+      customer: 'Amit Verma',
+      total: 3500,
+      status: 'cancelled',
+    },
   ];
+
+  constructor() {
+    this.checkScreen();
+  }
+
+  @HostListener('window:resize')
+  checkScreen() {
+    this.isMobile = window.innerWidth <= 900;
+    this.sidebarOpen = !this.isMobile; // auto collapse on mobile
+  }
 
   toggleSidebar() {
     this.sidebarOpen = !this.sidebarOpen;
   }
 
-
-delete(id:any){
-  
-}
-
+  delete(id: any) {}
 }
