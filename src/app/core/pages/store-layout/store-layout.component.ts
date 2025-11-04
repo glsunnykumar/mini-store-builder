4a76bf3611d2adcb3bb4670bf29766d34036612b
glsunnykumar <glsunny.kumar@gmail.com>
2025-11-04 14:10:30 +0530

    footer auto-hide on scroll down and reappear on scroll up

--- src/app/core/pages/store-layout/store-layout.component.ts
+++ src/app/core/pages/store-layout/store-layout.component.ts
@@ -1,13 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { StoreNavbarComponent } from '../store-navbar/store-navbar.component';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-store-layout',
-   imports: [RouterOutlet, StoreNavbarComponent],
+   imports: [RouterOutlet,
+     StoreNavbarComponent,
+     MatIconModule
+    ],
   templateUrl: './store-layout.component.html',
   styleUrl: './store-layout.component.scss'
 })
 export class StoreLayoutComponent {
+
+   currentYear = new Date().getFullYear();
 
 }
